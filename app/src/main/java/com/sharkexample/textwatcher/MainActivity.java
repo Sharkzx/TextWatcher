44ea6422d74658e0b5205998768661cecb393d43
Luiz Tadeu <tadeu.mlf@hotmail.com>
2016-05-01 22:10:10 -0300

    teste de commit TextWatcher

---
+++ app/src/main/java/com/sharkexample/textwatcher/MainActivity.java
@@ -0,0 +1,30 @@
+package com.sharkexample.textwatcher;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private  TextView tvCPF;
+    private  EditText etCPF;
+    private  TextWatcher     cpfMask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     tvCPF = (TextView) findViewById(R.id.lblTitleCPF);
+     etCPF = (EditText) findViewById(R.id.txtCPF);
+
+
+        cpfMask = textWatcherCPF.insert("###.###.###-##", etCPF);
+
+        etCPF.addTextChangedListener(cpfMask);
+
+
+
+    }
+}
